c5c9f205f84412cb9195d9db845ade173fffea47
myeongin <kai5588@naver.com>
2025-04-24 16:19:35 +0900

    3-Layered 구축

--- src/main/java/org/example/pg/presentation/PayController.java
+++ src/main/java/org/example/pg/presentation/PayController.java
@@ -1,6 +1,7 @@
 package org.example.pg.presentation;
 
 import lombok.extern.slf4j.Slf4j;
+import org.example.pg.service.PayService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,8 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @Controller
 public class PayController {
 
+    private final PayService payService;
+
+    public PayController(PayService payService) {
+        this.payService = payService;
+    }
+
     @GetMapping("")
     public String index() {
+        payService.init();
         return "index";
     }
 
---
+++ src/main/java/org/example/pg/service/PayService.java
@@ -0,0 +1,21 @@
+package org.example.pg.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Component
+public class PayService {
+    public void init() {
+        System.out.println("\n init \n");
+
+
+        System.out.println("\n EXIT \n");
+
+    }
+}
